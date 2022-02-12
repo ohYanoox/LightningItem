b26328ca1ff8f9caf62eb0ccd6f568b86190c407
ohYanoox <87778340+ohYanoox@users.noreply.github.com>
2022-02-12 20:43:30 +0100

    Create loader.php

---
+++ src/Yanoox/loader.php
@@ -0,0 +1,64 @@
+<?php
+/*
+__    __      ___   __   _   _____   _____  __    __
+\ \  / /     /   | |  \ | | /  _  \ /  _  \ \ \  / /
+ \ \/ /     / /| | |   \| | | | | | | | | |  \ \/ /
+  \  /     / / | | | |\   | | | | | | | | |   }  {
+  / /     / /  | | | | \  | | |_| | | |_| |  / /\ \
+ /_/     /_/   |_| |_|  \_| \_____/ \_____/ /_/  \_\
+
+Plugin's name: LightningItem
+Author: Yanoox
+Plugin's version: 0.1
+Plugin's api: 3.0.0
+For: everyone
+ */
+namespace Yanoox;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use Yanoox\Events\eventListener;
+
+class loader extends PluginBase{
+
+    public static loader $instance;
+
+    public function onEnable() : void
+    {
+        self::$instance = $this;
+        $this->initResources();
+        $this->getServer()->getPluginManager()->registerEvents(new eventListener(), $this);
+
+        $this->getLogger()->info("§3|━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━|");
+        $this->getLogger()->info("§3|The LightningItem plugin is loaded|");
+        $this->getLogger()->info("§3|━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━|");
+    }
+
+    public static function getInstance() : loader
+    {
+        return self::$instance;
+    }
+
+    public static function getConfigFile() : Config{
+        return new Config(self::getInstance()->getDataFolder() . "config.yml", Config::YAML);
+    }
+
+    public function initResources(): bool
+    {
+        @mkdir($this->getDataFolder());
+        @$this->saveResource("config.yml");
+        if (!file_exists($this->getDataFolder() . "config.yml")) {
+            $this->getLogger()->error("Cannot get the config file : config.yml");
+            return false;
+        }
+
+        return true;
+    }
+
+    public function onLoad() : void
+    {
+        $this->saveConfig();
+    }
+}
+
+
